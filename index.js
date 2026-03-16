f23d927c04207277a656957749e934e0955faa8c
Aryan Makashre <aryanmakashre.777@gmail.com>
2026-03-16 21:00:12 +0530

    backendcrud

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
